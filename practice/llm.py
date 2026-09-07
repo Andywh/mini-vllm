dd03b8936ea47bd8dc8ddb3ef393bbfd01743502
andy <isongliang@qq.com>
2026-09-07 23:51:51 +0800

    llm.py

---
+++ practice/llm.py
@@ -0,0 +1,27 @@
+from engine import MiniEngine
+from output_processor import MiniRequestOutput
+
+
+class MiniLLM:
+
+    def __init__(self) -> None:
+        self.engine = MiniEngine()
+
+    def generate(
+            self,
+            prompt_token_ids_list: list[list[int]],
+            max_tokens: int = 16,
+    ) -> list[MiniRequestOutput]:
+
+        for ids in prompt_token_ids_list:
+            self.engine.add_request(ids, max_tokens=max_tokens)
+
+        results: list[MiniRequestOutput] = []
+        while self.engine.has_unfinished_requests():
+            for out in self.engine.step():
+                if out.finished:
+                    results.append(out)
+
+        results.sort(key=lambda x: int(x.request_id))
+        return results
+
